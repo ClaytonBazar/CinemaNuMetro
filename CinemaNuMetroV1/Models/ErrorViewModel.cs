a132f87d6bf10ee32b3a693291b8b9cb8eede040
ClaytonBazar <claytonbazar04@gmail.com>
2025-04-11 17:57:01 +0200

    Adicionar arquivos de projeto.

---
+++ CinemaNuMetroV1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaNuMetroV1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
